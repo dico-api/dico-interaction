b31cf328e31a05d80d8a0d39ea278fc24af3c0cb
Eunwoo Choi <61371424+eunwoo1104@users.noreply.github.com>
2022-02-14 13:18:33 +0900

    Version Up

--- dico_interaction/__init__.py
+++ dico_interaction/__init__.py
@@ -10,7 +10,7 @@ from .client import InteractionClient
 from .command import InteractionCommand, AutoComplete, autocomplete
 from .component import ComponentCallback
 from .context import InteractionContext
-from .deco import command, slash, context_menu, component_callback, checks, option
+from .deco import command, slash, context_menu, component_callback, checks, option, modal_callback
 from .webserver import InteractionWebserver
 
-__version__ = "0.0.7"
+__version__ = "0.0.8"
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="UTF-8") as f:
 
 setuptools.setup(
     name="dico-interaction",
-    version="0.0.7",
+    version="0.0.8",
     author="eunwoo1104",
     author_email="sions04@naver.com",
     description="Interaction module for dico.",
