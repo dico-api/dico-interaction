97335168540b532c49465b7979ed47a2df9bae5a
Eunwoo Choi <61371424+eunwoo1104@users.noreply.github.com>
2022-02-14 13:17:40 +0900

    Use ComponentCallback instead of individual class

--- dico_interaction/modal.py
+++ dico_interaction/modal.py
@@ -1,12 +1,5 @@
-class ModalCallback:
-    def __init__(self, custom_id, coro):
-        self.custom_id = custom_id or coro.__name__
-        self.coro = coro
-        self.self_or_cls = None
+from .component import ComponentCallback
 
-    def register_self_or_cls(self, addon):
-        self.self_or_cls = addon
 
-    def invoke(self, interaction, *_, **__):
-        args = (interaction,) if self.self_or_cls is None else (self.self_or_cls, interaction)
-        return self.coro(*args)
+class ModalCallback(ComponentCallback):
+    pass
